ec980df7d8af50f7743db5ece15e42e8c1f6d611
gaojunya <gaojunya@xinhuamm.net>
2016-08-30 15:09:56 +0800

    fix

--- sugar/videofix/videofix.js
+++ sugar/videofix/videofix.js
@@ -1,48 +1,90 @@
 // 播放器小窗口组件
 (function(sugarname){
-  var videofix = function(width,height,position) {
-    this.width=width;
-    this.height=height;
-    this.position=position;
+  var videofix = function(obj, config) {
+    this.config = {
+      'position': 'left bottom', // 位置：默认 左上
+      'width': '50%',
+      'height': 'auto'
+    };
+    if (config) {
+      $.extend(this.config, config);
+    }
+    if (typeof(obj) == 'object') {
+      this.videoObj = obj;
+    } else {
+      this.videoObj = $('#'+obj);
+    }
+    this.init();
   };
   videofix.prototype = {
-    init:function(){
-      $('.video').css({
-        'width':window.innerWidth,
-        'height':'18%',
-        'position':'relative'
-      })
-      $('video').css('width','100%');
-      var divWidth=$('.video').width();
-      var divHeight=$('.video').height();
-      var videoWidth=$('video').width();
-      var videoHeight=$('video').height();
-      var height=window.innerHeight;
-      var config={};
-      //位置
-      if(this.position){
-        var val=this.position;
-        var arr=val.split('-');
-        if(val=='left-top'||val=='left-bottom'||val=='right-top'||val=='right-bottom'){
-          config[arr[0]]=0;
-          config[arr[1]]=0;
-        }else if(val=='left-center'||val=='right-center'){
-          config[arr[0]]=0;
-          config['margin-top']=(height-videoHeight)/2+'px';
-        }
+    init: function(){
+      // 获取播放器宽高
+      this.videoWidth = this.videoObj.width();
+      this.videoHeight = this.videoObj.height();
+      console.log('videoWidth:'+this.videoWidth);
+      console.log('videoHeight:'+this.videoHeight);
+      // 计算缩小后宽高
+      if (this.config['width'].indexOf('%') > -1) {
+        console.log('fixWidth:'+this.videoWidth * parseInt(this.config['width']) / 100);
+        this.fixVideoWidth = this.videoWidth * parseInt(this.config['width']) / 100;
+      } else {
+        this.fixVideoWidth = this.config['width'];
       }
-      setInterval(function(){
-        if($('body').scrollTop()>=divHeight){
-          var videoCss=$.extend({'width':this.width,'position':'fixed'},config);
-          $('video').css(videoCss);
-        }else{
-          $('video').css({
-            'width':divWidth,
-            'position':'absolute',
-            'margin-top':'0'
-          })
+      if (this.config['height'] != 'auto') {
+        if (this.config['height'].indexOf('%') > -1) {
+          console.log('fixHeight:'+this.videoHeight *parseInt(this.config['height']) / 100);
+          this.fixVideoHeight = this.videoHeight * parseInt(this.config['height']) / 100;
+        }else {
+          this.fixVideoHeight = this.config['height'];
         }
-      })
+      }else{
+        console.log('fixHeight:'+this.fixVideoHeight);
+        this.fixVideoHeight = this.videoObj.height();
+      }
+      // 计算缩小后位置
+      var _this=this;
+      var config={};
+      var width=window.innerWidth,
+          height=window.innerHeight;
+      if(this.config['position']!=''){
+        var val=this.config['position'].split(' ');
+        if(val[0]=='left'){
+          config['left']=0;
+        }else if(val[0]=='right'){
+          config['right']=0;
+        }else if(val[0]=='center'){
+          config['margin-left']=(width-_this.fixVideoWidth)/2+'px';
+        }else{
+          config['left']=val[0];
+        }
+        if(val[1]=='top'){
+          config['top']=0;
+        }else if(val[1]=='bottom'){
+          config['bottom']=0;
+        }else if(val[1]==null||val[1]=='center'){
+          config['margin-top']=(height-_this.fixVideoHeight)/2+'px';
+        }else{
+          config['top']=val[1];
+        }
+        console.log(config);
+      }
+      // 绑定滚动条
+      $(document).scroll(function(){
+        if($('body').scrollTop()>=_this.videoHeight){
+          var videofixCss=$.extend({'width':_this.config['width'],'height':_this.config['height'],'position':'fixed'},config);
+          $(_this.videoObj).css(videofixCss);
+        }else{
+          $(_this.videoObj).css({
+            'width':_this.videoWidth,
+            'height':'auto',
+            'position':'absolute',
+            'top':'0',
+            'left':'0',
+            'margin-top':'0',
+            'margin-left':'0'
+          });
+        }
+      });
     }
   };
   sugar[sugarname] = videofix;
